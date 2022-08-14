6f4d05eae3d882fc1822f86412acfab5ce7c94e7
Connor Spear <cspeardev@gmail.com>
2022-08-14 17:05:47 -0400

    Change implementation of testing data. Add documentation comments.

--- IP Address Utilities Tests/ExtendedIPAddressTests.cs
+++ IP Address Utilities Tests/ExtendedIPAddressTests.cs
@@ -1,53 +1,54 @@
-﻿using IPAddressUtilities;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+﻿using IP_Address_Utilities_Tests;
+using IPAddressUtilities;
+using Xunit;
+using static Xunit.Assert;
+
+//using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
 
 namespace IPAddressUtilitiesTests
 {
-    [TestClass]
+    //[TestClass]
     public class ExtendedIPAddressTests
     {
-        [TestMethod]
-        public void NotEqualTest()
+        [Theory]
+        [MemberData(nameof(NotEqualTestData.TestData), MemberType = typeof(NotEqualTestData))]
+        public void NotEqualTest(ExtendedIPAddress upper, ExtendedIPAddress lower)
         {
-            ExtendedIPAddress lower = new("10.0.0.1");
-            ExtendedIPAddress upper = new("10.0.0.2");
-            
-            AreNotEqual(upper, lower);
+            NotEqual(upper, lower);
         }
 
 
-        [TestMethod]
-        public void EqualTest()
-        {
-            ExtendedIPAddress address = new("10.0.0.1");
-            ExtendedIPAddress address2 = new("10.0.0.1");
+        //[TestMethod]
+        //public void EqualTest()
+        //{
+        //    ExtendedIPAddress address = new("10.0.0.1");
+        //    ExtendedIPAddress address2 = new("10.0.0.1");
 
-            AreEqual(address, address2);
-            IsTrue(address == address2);
-        }
+        //    AreEqual(address, address2);
+        //    IsTrue(address == address2);
+        //}
 
 
-        [TestMethod]
-        public void IncrementTest()
-        {
-            ExtendedIPAddress address = new("10.0.0.1");
-            address++;
-            AreEqual(new("10.0.0.2"), address);
+        //[TestMethod]
+        //public void IncrementTest()
+        //{
+        //    ExtendedIPAddress address = new("10.0.0.1");
+        //    address++;
+        //    AreEqual(new("10.0.0.2"), address);
 
-            address = new("10.0.0.255");
-            address++;
-            AreEqual(new("10.0.1.0"), address);
-        }
+        //    address = new("10.0.0.255");
+        //    address++;
+        //    AreEqual(new("10.0.1.0"), address);
+        //}
 
-        [TestMethod]
-        public void DecrementTest()
-        {
-
-            ExtendedIPAddress address = new("10.0.0.2");
-            address--;
-            AreEqual(address, new("10.0.0.1"));
-        }
+        //[TestMethod]
+        //public void DecrementTest()
+        //{
+        //    ExtendedIPAddress address = new("10.0.0.2");
+        //    address--;
+        //    AreEqual(address, new("10.0.0.1"));
+        //}
 
     }
 }
--- IP Address Utilities/ExtendedIPAddress.cs
+++ IP Address Utilities/ExtendedIPAddress.cs
@@ -1,200 +1,268 @@
 ﻿using System.Net;
 using System.Numerics;
 
-namespace IPAddressUtilities
+namespace IPAddressUtilities;
+
+/// <summary>
+/// Extension of <see cref="IPAddress"/> that adds extra functionality
+/// </summary>
+public class ExtendedIPAddress : IPAddress, IEquatable<ExtendedIPAddress>, IComparable<ExtendedIPAddress>
 {
-    public class ExtendedIPAddress : IPAddress, IEquatable<ExtendedIPAddress>, IComparable<ExtendedIPAddress>
+    #region constructors
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="address"></param>
+    public ExtendedIPAddress(byte[] address) : base(address)
     {
-        #region constructors
-        public ExtendedIPAddress(byte[] address) : base(address)
-        {
-        }
-
-        public ExtendedIPAddress(byte[] address, long scopeid) : base(address, scopeid)
-        {
-        }
-
-        public ExtendedIPAddress(long newAddress) : base(newAddress)
-        {
-        }
-
-        public ExtendedIPAddress(ReadOnlySpan<byte> address) : base(address)
-        {
-        }
-
-        public ExtendedIPAddress(ReadOnlySpan<byte> address, long scopeid) : base(address, scopeid)
-        {
-        }
-
-        public ExtendedIPAddress(string stringAddress): base(Parse(stringAddress).GetAddressBytes())
-        {
-        }
-        #endregion constructors
-
-        public static bool operator >(ExtendedIPAddress a, ExtendedIPAddress b)
-        {
-            return a.CompareTo(b) > 0;
-        }
-
-        public static bool operator >=(ExtendedIPAddress a, ExtendedIPAddress b)
-        {
-            return (a.CompareTo(b) > 0 || a.CompareTo(b) == 0);
-        }
-
-        public static bool operator <(ExtendedIPAddress a, ExtendedIPAddress b)
-        {
-            return (a.CompareTo(b) < 0);
-        }
-
-        public static bool operator <=(ExtendedIPAddress a, ExtendedIPAddress b)
-        {
-            return (a.CompareTo(b) < 0 || a.CompareTo(b) == 0);
-        }
-
-        public static ExtendedIPAddress operator ++(ExtendedIPAddress a)
-        {
-            int tetCount = a.GetAddressBytes().Length;
-            ExtendedIPAddress incrementedAddress;
-
-            BigInteger addressInt = ConvertIPAddressBits(a);
-            addressInt++;
-            incrementedAddress = ConvertBitsToAddress(addressInt, tetCount);
-            return incrementedAddress;
-        }
-
-        public static ExtendedIPAddress operator --(ExtendedIPAddress a)
-        {
-            int tetCount = a.GetAddressBytes().Length;
-            ExtendedIPAddress decrementedAddress;
-
-            BigInteger addressInt = ConvertIPAddressBits(a);
-            addressInt--;
-            decrementedAddress = ConvertBitsToAddress(addressInt, tetCount);
-            return decrementedAddress;
-        }
-
-        private static ExtendedIPAddress ConvertBitsToAddress(BigInteger bits, int tetCount)
-        {
-            byte[] addressBytes = new byte[tetCount];
-            for (int i = 0; i < tetCount; i++)
-            {
-                addressBytes[i] = (byte)((bits
-                    >> (8 * (tetCount - (i + 1)))) & 0xFF);
-            }
-            return new ExtendedIPAddress(addressBytes);
-        }
-
-        private static BigInteger ConvertIPAddressBits(ExtendedIPAddress address)
-        {
-            int tetCount = address.GetAddressBytes().Length;
-            int tetBits = address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork ? 8 : 16;
-            int groupBytesSize = tetBits / 8;
-
-            byte[] addressBytes = address.GetAddressBytes();
-
-            BigInteger addressBits = 0;
-
-            for (int i = 0; i < (tetCount / groupBytesSize); i++)
-            {
-                BigInteger groupBits = GetGroupBits(addressBytes, groupBytesSize, i);
-                addressBits |= groupBits << (tetBits * ((tetCount / groupBytesSize) - (i + 1)));
-            }
-            return addressBits;
-        }
-
-        private static BigInteger GetGroupBits(byte[] addressBytes, int groupByteSize, int startIndex)
-        {
-            BigInteger groupBytes = 0;
-
-            startIndex *= groupByteSize;
-
-            for (int i = 0; i < groupByteSize; i++)
-            {
-                groupBytes |= addressBytes[((startIndex) + groupByteSize) - (i + 1)] << (8 * (i));
-            }
-            return groupBytes;
-        }
-        
-        private static BigInteger CompareIPAddresses(ExtendedIPAddress firstAddress, ExtendedIPAddress secondAddress)
-        {
-            BigInteger difference;
-
-            BigInteger firstAddressBits = ConvertIPAddressBits(firstAddress);
-            BigInteger secondAddressBits = ConvertIPAddressBits(secondAddress);
-
-            difference = firstAddressBits - secondAddressBits;
-
-            return difference;
-        }
-
-        public bool Equals(ExtendedIPAddress? other)
-        {
-            if (other is null)
-            {
-                return false;
-            }
-            return CompareIPAddresses(this, other) == 0;
-        }
-
-        public int CompareTo(ExtendedIPAddress? other)
-        {
-            ArgumentNullException.ThrowIfNull(other);
-            var comparison = CompareIPAddresses(this, other);
-            if(comparison > 0)
-            {
-                return 1;
-            }
-            else if(comparison < 0)
-            {
-                return -1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
-        public override bool Equals(object? comparand)
-        {
-            if (ReferenceEquals(this, comparand))
-            {
-                return true;
-            }
-
-            if (comparand is null)
-            {
-                return false;
-            }
-
-            if(comparand.GetType() != typeof(ExtendedIPAddress))
-            {
-                return false;
-            }
-
-            var other = (ExtendedIPAddress)comparand;
-
-            return CompareTo(other) == 0;
-        }
-
-        public override int GetHashCode()
-        {
-            throw new NotImplementedException();
-        }
-
-        public static bool operator ==(ExtendedIPAddress left, ExtendedIPAddress right)
-        {
-            if (left is null)
-            {
-                return right is null;
-            }
-
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(ExtendedIPAddress left, ExtendedIPAddress right)
-        {
-            return !(left == right);
-        }
-
     }
+
+    public ExtendedIPAddress(byte[] address, long scopeid) : base(address, scopeid)
+    {
+    }
+
+    public ExtendedIPAddress(long newAddress) : base(newAddress)
+    {
+    }
+
+    public ExtendedIPAddress(ReadOnlySpan<byte> address) : base(address)
+    {
+    }
+
+    public ExtendedIPAddress(ReadOnlySpan<byte> address, long scopeid) : base(address, scopeid)
+    {
+    }
+
+    public ExtendedIPAddress(string stringAddress): base(Parse(stringAddress).GetAddressBytes())
+    {
+    }
+    #endregion constructors
+
+    #region operators
+    /// <summary>
+    /// Greater than operator.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool operator >(ExtendedIPAddress a, ExtendedIPAddress b)
+    {
+        return a.CompareTo(b) > 0;
+    }
+    /// <summary>
+    /// Greater than or equal to operator.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool operator >=(ExtendedIPAddress a, ExtendedIPAddress b)
+    {
+        return (a.CompareTo(b) > 0 || a.CompareTo(b) == 0);
+    }
+
+    /// <summary>
+    /// Less than operator.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool operator <(ExtendedIPAddress a, ExtendedIPAddress b)
+    {
+        return (a.CompareTo(b) < 0);
+    }
+    /// <summary>
+    /// Less than or equal to operator.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool operator <=(ExtendedIPAddress a, ExtendedIPAddress b)
+    {
+        return (a.CompareTo(b) < 0 || a.CompareTo(b) == 0);
+    }
+    /// <summary>
+    /// Increment operator.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <returns></returns>
+    public static ExtendedIPAddress operator ++(ExtendedIPAddress a)
+    {
+        int tetCount = a.GetAddressBytes().Length;
+        ExtendedIPAddress incrementedAddress;
+
+        BigInteger addressInt = ConvertIPAddressBits(a);
+        addressInt++;
+        incrementedAddress = ConvertBitsToAddress(addressInt, tetCount);
+        return incrementedAddress;
+    }
+    /// <summary>
+    /// Decrement operator.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <returns></returns>
+    public static ExtendedIPAddress operator --(ExtendedIPAddress a)
+    {
+        int tetCount = a.GetAddressBytes().Length;
+        ExtendedIPAddress decrementedAddress;
+
+        BigInteger addressInt = ConvertIPAddressBits(a);
+        addressInt--;
+        decrementedAddress = ConvertBitsToAddress(addressInt, tetCount);
+        return decrementedAddress;
+    }
+    /// <summary>
+    /// Converts provided bits into new instance of <see cref="ExtendedIPAddress"/>.
+    /// </summary>
+    /// <param name="bits"></param>
+    /// <param name="tetCount"></param>
+    /// <returns></returns>
+    private static ExtendedIPAddress ConvertBitsToAddress(BigInteger bits, int tetCount)
+    {
+        byte[] addressBytes = new byte[tetCount];
+        for (int i = 0; i < tetCount; i++)
+        {
+            addressBytes[i] = (byte)((bits
+                >> (8 * (tetCount - (i + 1)))) & 0xFF);
+        }
+        return new ExtendedIPAddress(addressBytes);
+    }
+    /// <summary>
+    /// Equal to operator.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static bool operator ==(ExtendedIPAddress left, ExtendedIPAddress right)
+    {
+        if (left is null)
+        {
+            return right is null;
+        }
+        return left.Equals(right);
+    }
+    /// <summary>
+    /// Not equal to operator.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public static bool operator !=(ExtendedIPAddress left, ExtendedIPAddress right)
+    {
+        return !(left == right);
+    }
+    #endregion
+
+    /// <summary>
+    /// Sums the bits of <paramref name="address"/> into a BigInt.
+    /// </summary>
+    /// <param name="address"></param>
+    /// <returns></returns>
+    private static BigInteger ConvertIPAddressBits(ExtendedIPAddress address)
+    {
+        int tetCount = address.GetAddressBytes().Length;
+        int tetBits = address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork ? 8 : 16;
+        int groupBytesSize = tetBits / 8;
+
+        byte[] addressBytes = address.GetAddressBytes();
+
+        BigInteger addressBits = 0;
+
+        for (int i = 0; i < (tetCount / groupBytesSize); i++)
+        {
+            BigInteger groupBits = GetGroupBits(addressBytes, groupBytesSize, i);
+            addressBits |= groupBits << (tetBits * ((tetCount / groupBytesSize) - (i + 1)));
+        }
+        return addressBits;
+    }
+    private static BigInteger GetGroupBits(byte[] addressBytes, int groupByteSize, int startIndex)
+    {
+        BigInteger groupBytes = 0;
+
+        startIndex *= groupByteSize;
+
+        for (int i = 0; i < groupByteSize; i++)
+        {
+            groupBytes |= addressBytes[((startIndex) + groupByteSize) - (i + 1)] << (8 * (i));
+        }
+        return groupBytes;
+    }
+    
+    /// <summary>
+    /// Compares two IP addresses.
+    /// </summary>
+    /// <param name="firstAddress"></param>
+    /// <param name="secondAddress"></param>
+    /// <exception cref="ArgumentNullException"/>
+    /// <returns>The number of addresses between <paramref name="firstAddress"/> and <paramref name="secondAddress"/></returns>
+    private static BigInteger CompareIPAddresses(ExtendedIPAddress firstAddress, ExtendedIPAddress secondAddress)
+    {
+        ArgumentNullException.ThrowIfNull(firstAddress, nameof(firstAddress));
+        ArgumentNullException.ThrowIfNull(secondAddress, nameof(secondAddress));
+
+        BigInteger difference;
+
+        BigInteger firstAddressBits = ConvertIPAddressBits(firstAddress);
+        BigInteger secondAddressBits = ConvertIPAddressBits(secondAddress);
+
+        difference = firstAddressBits - secondAddressBits;
+
+        return difference;
+    }
+
+    public bool Equals(ExtendedIPAddress? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+        return CompareIPAddresses(this, other) == 0;
+    }
+
+    public int CompareTo(ExtendedIPAddress? other)
+    {
+        ArgumentNullException.ThrowIfNull(other);
+        var comparison = CompareIPAddresses(this, other);
+        if(comparison > 0)
+        {
+            return 1;
+        }
+        else if(comparison < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public override bool Equals(object? comparand)
+    {
+        if (ReferenceEquals(this, comparand))
+        {
+            return true;
+        }
+
+        if (comparand is null)
+        {
+            return false;
+        }
+
+        if(comparand.GetType() != typeof(ExtendedIPAddress))
+        {
+            return false;
+        }
+
+        var other = (ExtendedIPAddress)comparand;
+
+        return CompareTo(other) == 0;
+    }
+
+    new public BigInteger GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
+
+    
+
 }
